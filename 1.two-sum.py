9b009967be5fad8598ad59a55cb974dd47132243
sankalpgunturi <sgunturi@protonmail.com>
2022-04-02 14:46:11 +0530

    A small change after testing my code.

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -1,36 +1,33 @@
-nums = [2, 7, 11, 15]
-target = 9
-final_list = []
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        final_list = []
 
-nums.sort()
+        new_nums = nums.sort()
 
-def add_digits(digit):
-    i = len(nums) - 2
-    while i >= 0:
-        if target == digit + nums[i - 1]:
-            return final_list.append(nums[i - 1])
+        def add_digits(digit):
+            i = len(nums) - 2
+            while i >= 0:
+                if target == digit + nums[i - 1]:
+                    return final_list.append(nums[i - 1])
+
+                else:
+                    i = i - 1
+
+        def closest(lst, K):
+
+            return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
+
+        if target > nums[len(nums) - 1]:
+            digit = nums[len(nums) - 1]
+            add_digits(digit)
+            final_list.append(digit) 
+            return [final_list.index(final_list[0]), final_list.index(final_list[1])]
+
+        elif target < nums[len(nums) - 1]:
+            digit = closest(nums, target)
+            add_digits(digit)
+            final_list.append(digit)
+            return [final_list.index(final_list[0]), final_list.index(final_list[1])]
 
         else:
-            i = i - 1
-
-def closest(lst, K):
-
-    return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
-
-if target > nums[len(nums) - 1]:
-    digit = nums[len(nums) - 1]
-    add_digits(digit)
-    final_list.append(digit) 
-
-elif target < nums[len(nums) - 1]:
-    digit = closest(nums, target)
-    add_digits(digit)
-    final_list.append(digit)
-
-else:
-    final_list.append(nums[0])
-    final_list.append([len(nums) - 1])
-
-for i in range(0, len(final_list)):
-
-    print(final_list)
+            return [nums[0], [len(nums) - 1]]
